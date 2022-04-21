c645dbb4a2fb52cb86c0afe585f8d14cb085b676
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-21 09:46:20 +0530

    [Sunny] Add : NLog File Added

---
+++ NlogDemo/AddNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NlogDemo
+{
+    public class AddNumber
+    {
+        private readonly Nlog nLog = new Nlog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            ////Checking values equal to 0
+            if (firstNumber == 0 || secondNumber == 0)
+            {
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogError("Expecting not null values");
+                nLog.LogWarn("FirstNumber or secondNumber should not equal to 0");
+            }
+            int result = secondNumber + secondNumber;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum Method passed, Result" + result);
+            return result;
+        }
+    }
+}
---
+++ NlogDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NLog;
+using NlogDemo;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To NLog!");
+            AddNumber add = new AddNumber();
+            add.Sum(1, 2);
+
+        }
+    }
+}
